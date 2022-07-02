e1e11bcf2c2b2b0524492bf1cdbc787d665444ef
Laksminta Sasti <aku.laksminta.sasti@gmail.com>
2022-07-02 17:40:23 +0700

    login

---
+++ src/main/java/LoginAccount.java
@@ -0,0 +1,22 @@
+import org.junit.Test;
+import org.openqa.selenium.By;
+
+
+import static org.junit.Assert.assertEquals;
+
+
+public class LoginAccount extends beforeafter{
+    @Test
+    public void login() throws InterruptedException {
+        //User membuka website booking hotel
+        driver.get("http://qa.cilsy.id:8080/en/");
+
+        driver.findElement(By.xpath("//*[@id=\"header\"]/div[3]/div/div/div[7]/ul/li/a")).click();
+        driver.findElement(By.id("email")).sendKeys("testing@test.com");
+        driver.findElement(By.id("passwd")).sendKeys("123456");
+        driver.findElement(By.id("SubmitLogin")).click();
+
+        Thread.sleep(3000);
+    }
+}
+
--- src/main/java/beforeafter.java
+++ src/main/java/beforeafter.java
@@ -1,6 +1,5 @@
 import org.junit.After;
 import org.junit.Before;
-import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
